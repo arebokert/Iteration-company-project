ff4d195270a291c77aa9d7d7cf1e8d8884489dcb
Erik Mårtensson <erima618@li15-13.ida.liu.se>
2015-10-06 11:27:34 +0200

    Fixed errors in run.sh and delTapResults.sh

--- tests/run.sh
+++ tests/run.sh
@@ -1,7 +1,7 @@
 cd tests
 for f in $(find . ! -name "luaunit.lua" -name '*.lua')
 do
-	/var/lib/jenkins/lua "$f" -o tap >> $f".tap"
+	/var/lib/jenkins/bin/lua "$f" -o tap >> $f".tap"
 done
 #/var/lib/jenkins/bin/lua  -o tap >> $f
 #/var/lib/jenkins/bin/lua tests/test.lua -o tap
