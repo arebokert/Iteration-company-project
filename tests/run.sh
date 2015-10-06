d1daf65060ae2ada733596b84860c82905016853
Erik Mårtensson <erima618@li15-13.ida.liu.se>
2015-10-06 10:48:31 +0200

    Further changes made to run.sh to support recursive directories

--- tests/run.sh
+++ tests/run.sh
@@ -1,7 +1,7 @@
 cd tests
-for f in *.lua
+for f in $(find . ! -name "luaunit.lua" -name '*.lua')
 do
-	if [ "$f" != "luaunit.lua" ]; then /var/lib/jenkins/bin/lua "$f" -o tap >> $f".tap"; fi;
+	/var/lib/jenkins/bin/lua "$f" -o tap >> $f".tap"
 done
 #/var/lib/jenkins/bin/lua  -o tap >> $f
 #/var/lib/jenkins/bin/lua tests/test.lua -o tap
