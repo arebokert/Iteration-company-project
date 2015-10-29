9802e06434101eebbf2c30b19d666e61447b046f
Gustaf Olofsson <gusol745@student.liu.se>
2015-10-29 14:46:11 +0100

    using luaunit to try to find path 2

--- tests/apitest.lua
+++ tests/apitest.lua
@@ -2,15 +2,15 @@ luaunit = require('luaunit')
 
 local pathOfThisFile = ...
 local folderOfThisFile = (...):match("(.-)[^%.]+$")
-local oneFolderUp = folderOfThisFile:match("(.-)[^%.]+$")
+--local oneFolderUp = folderOfThisFile:match("(.-)[^%.]+$")
 
 -- this gives nil value on onefolderup it seems.
-gfx = require(oneFolderUp .. "src/SDK.Simulator.gfx")
+-- gfx = require(oneFolderUp .. "src/SDK.Simulator.gfx")
 
 function apitest()
 
-gfx.update()
-luaunit.assertEquals(1,1)
+-- gfx.update()
+luaunit.assertEquals(folderOfThisFile,"/tests")
 
 end
 
