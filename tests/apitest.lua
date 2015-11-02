58deb12463232e72768e13ea7e21be8159e697ad
Gustaf Olofsson <gusol745@student.liu.se>
2015-11-02 16:18:58 +0100

    needed to add references to every file in the simulator folder as they seem dependent on each other.

--- tests/apitest.lua
+++ tests/apitest.lua
@@ -4,6 +4,10 @@ package.path = package.path .. ";../src/SDK/Simulator/?.lua"
 
 gfx = require('gfx')
 player = require('player')
+zto = require('zto')
+surface = require('surface')
+freetype = require('freetype')
+sys = require('sys')
 
 function apitest()
 
