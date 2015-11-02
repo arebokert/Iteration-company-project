c7075552f3dc5896df3083f11806b3583b206211
Gustaf Olofsson <gusol745@student.liu.se>
2015-11-02 15:27:05 +0100

    Using old test file to see if Jenkins actually performs tests, as it didnt not with newly created tests

--- tests/apitest.lua
+++ tests/apitest.lua
@@ -1,17 +1,54 @@
 luaunit = require('luaunit')
 
-local pathOfThisFile = ...
-local folderOfThisFile = (...):match("(.-)[^%.]+$")
---local oneFolderUp = folderOfThisFile:match("(.-)[^%.]+$")
+--local function main()
+  --print("hello world")
 
--- this gives nil value on onefolderup it seems.
--- gfx = require(oneFolderUp .. "src/SDK.Simulator.gfx")
+--end
+--main()
 
-function apitest()
+--os.exit(luaunit.LuaUnit.run())
 
--- gfx.update()
-luaunit.assertEquals(folderOfThisFile,"/tests")
+-- Unit testing starts
+function add(v1,v2)
+    -- add positive numbers
+    -- return 0 if any of the numbers are 0
+    -- error if any of the two numbers are negative
+    if v1 < 0 or v2 < 0 then
+        error('Can only add positive or null numbers, received '..v1..' and '..v2)
+    end
+    if v1 == 0 or v2 == 0 then
+        return 0
+    end
+    return v1+v2
+end
 
+function testAddPositive()
+    luaunit.assertEquals(add(1,1),2)
+end
+
+function testAddZero()
+    luaunit.assertEquals(add(1,0),0)
+    luaunit.assertEquals(add(0,5),0)
+    luaunit.assertEquals(add(0,0),0)
 end
 
 os.exit( luaunit.LuaUnit.run() )
+
+
+--luaunit = require('luaunit')
+--
+--local pathOfThisFile = ...
+--local folderOfThisFile = (...):match("(.-)[^%.]+$")
+----local oneFolderUp = folderOfThisFile:match("(.-)[^%.]+$")
+--
+---- this gives nil value on onefolderup it seems.
+---- gfx = require(oneFolderUp .. "src/SDK.Simulator.gfx")
+--
+--function apitest()
+--
+---- gfx.update()
+--luaunit.assertEquals(folderOfThisFile,"/tests")
+--
+--end
+--
+--os.exit( luaunit.LuaUnit.run() )
