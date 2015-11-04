2d4a8291bd4f4b31e83bd1dee23c4e0a85d69c25
Erik Mårtensson <erima618@student.liu.se>
2015-11-04 09:45:19 +0100

    Apitest updated with working path

--- tests/apitest.lua
+++ tests/apitest.lua
@@ -1,19 +1,29 @@
 luaunit = require('luaunit')
 
-package.path = package.path .. ";../src/SDK/Simulator/?.lua"
+package.path = package.path .. ";../src/?.lua"
 
-gfx = require('gfx')
-player = require('player')
-zto = require('zto')
-surface = require('surface')
-freetype = require('freetype')
-sys = require('sys')
+ADConfig = require("Config.ADConfig")
+ADLogger = require("SDK.Utils.ADLogger")
+ADLogger.trace("Applicatio Init")
+
+root_path = ""
+if ADConfig.isSimulator then
+    gfx = require "SDK.Simulator.gfx"
+    zto = require "SDK.Simulator.zto"
+    surface = require "SDK.Simulator.surface"
+    player = require "SDK.Simulator.player"
+    freetype = require "SDK.Simulator.freetype"
+    sys = require "SDK.Simulator.sys"
+    root_path = ""
+else
+    root_path = sys.root_path()
+end
 
 function apitest()
 
-gfx.update()
-player.get_state()
-luaunit.assertEquals()
+--gfx.update()
+--player.get_state()
+--luaunit.assertEquals()
 
 end
 
