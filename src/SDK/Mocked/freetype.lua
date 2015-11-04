0fa240a219288c01f5398b4569f84f1e8fdc4f7d
Gustaf Olofsson <gusol745@student.liu.se>
2015-11-04 11:34:22 +0100

    created structure for mocking api. Pre-changes in main

---
+++ src/SDK/Mocked/freetype.lua
@@ -0,0 +1,16 @@
+local class = require( "SDK.Lib.classy" )
+
+local freetype = class( "SDK.Mocked.freetype")
+
+
+function freetype:__init()
+end
+
+function freetype:__init(fontColor, fontSize, drawingStartPoint, fontPath)
+end
+
+function freetype:draw_over_surface(sur, text)
+  end
+
+return freetype
+
--- tests/apitest.lua
+++ tests/apitest.lua
@@ -2,22 +2,11 @@ luaunit = require('luaunit')
 
 package.path = package.path .. ";../src/?.lua"
 
-ADConfig = require("Config.ADConfig")
+--ADConfig = require("Config.ADConfig")
 --ADLogger = require("SDK.Utils.ADLogger")
 --ADLogger.trace("Applicatio Init")
 
-root_path = ""
-if ADConfig.isSimulator then
-    gfx = require "SDK.Simulator.gfx"
-    zto = require "SDK.Simulator.zto"
-    surface = require "SDK.Simulator.surface"
-    player = require "SDK.Simulator.player"
-    freetype = require "SDK.Simulator.freetype"
-    sys = require "SDK.Simulator.sys"
-    root_path = ""
-else
-    root_path = sys.root_path()
-end
+freetype = require("SDK.Mocked.freetype")
 
 function apitest()
 
