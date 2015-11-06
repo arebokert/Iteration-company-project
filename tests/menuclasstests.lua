d1f5f4dfeda95a7d194132706c616924245f978d
Gustaf Romell <gusro855@student.liu.se>
2015-11-06 15:21:28 +0100

    test file for unit tests of menuclass was created

---
+++ tests/menuclasstests.lua
@@ -0,0 +1,22 @@
+package.path = package.path .. ";../src/model/mainmenu/?.lua"
+
+EXPORT_ASSERT_TO_GLOBALS = true;
+
+require('luaunit') -- always start a test file with this line
+
+require('menuclass')
+
+function testCrashMenuclass()
+  assertTrue(runMenuclassFunctions())
+end
+
+function runMenuclassFunctions()
+  value = Menu:new ()
+  --Menu:setOptions({1}) --fungerar inte pga ADlogger
+  --Menu:setActive(1)
+  --Menu:next()
+  --Menu:prev()
+  return true
+end
+
+os.exit( LuaUnit.run() ) -- always end a test file with this line
