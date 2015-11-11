6ca1b8fa716c4834170e427303fcc73eb63db075
GripDidrik <DidrikGrip@c-227-66.eduroam.liu.se>
2015-11-11 10:46:04 +0100

    Oops, was not supposed to be there.

--- src/model/highscore/highscore.lua
+++ src/model/highscore/highscore.lua
@@ -1,10 +1,11 @@
 -- The highscore object.
-Highscore = {index = 0, playerName = "", score = 0}
+Highscore = {playerName = "", score = 0}
 Highscore.__index = Highscore
 
 -- The constructor for the highscore-object
 -- @param playerName - The playername associated with the score
 -- @param score - The score
+-- @return Highscore - The object created
 function Highscore:new (playerName, score)
   local self = setmetatable({}, self)
   self.playerName = playerName
