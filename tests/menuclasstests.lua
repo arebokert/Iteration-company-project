f29df006e5b8dc1cda12e5d08ce78de9899d09b4
Gustaf Romell <gusro855@student.liu.se>
2015-11-11 11:46:12 +0100

    updated menuclasstest.lua with mocked API

--- tests/menuclasstests.lua
+++ tests/menuclasstests.lua
@@ -1,22 +1,46 @@
-package.path = package.path .. ";../src/model/mainmenu/?.lua"
+-- the file consists of unit tests testing the functions inte the file menuclass.lua
+
+-- getting us to the appropriate folder
+package.path = package.path .. ";../src/?.lua"
 
 EXPORT_ASSERT_TO_GLOBALS = true;
 
-require('luaunit') -- always start a test file with this line
+-- always start a test file with this line
+require('luaunit')
 
-require('menuclass')
+-- changeing the varible ADLogger to the mocked version
+-- makes it possible to run the Zenterio API-function ADLogger in the command prompt without crashing
+ADLogger = require("SDK.Mocked.ADLogger")
+gfx = require('SDK.Mocked.gfx')
 
+mainMenuContainer = gfx.new_surface(10, 10)
+
+
+-- requireing the file which we're testing 
+require('model.mainmenu.menuclass')
+
+
+-- the acutal test function, test if subfunction runMenuclassFunctions() doesn't cranch and returns true 
 function testCrashMenuclass()
   assertTrue(runMenuclassFunctions())
 end
 
+-- run all the functions in the menuclass.lua
+-- returns true if non of the functions crash
 function runMenuclassFunctions()
   value = Menu:new ()
-  --Menu:setOptions({1}) --fungerar inte pga ADlogger
+  Menu:setOptions({1}) --fungerar inte pga ADlogger
+  
+  -- crashes because gfx-mock doesn't return proper values
+  -- x = mainMenuContainer:get_width()
+  --Menu:print(mainMenuContainer, 1, 1, 1) 
   --Menu:setActive(1)
   --Menu:next()
-  --Menu:prev()
+  -- Menu:prev()
+  -- Menu:action()
+  -- Menu:hover()
   return true
 end
 
-os.exit( LuaUnit.run() ) -- always end a test file with this line
+-- always end a test file with this line
+os.exit( LuaUnit.run() )
