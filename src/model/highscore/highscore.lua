8bca5cbf6f52317f7bf1c0b594916caf513e0513
GripDidrik <DidrikGrip@c-227-66.eduroam.liu.se>
2015-11-11 10:44:12 +0100

    Extended the highscoreHandler, you can now select how many scores you want to fetch in the constructor. Also added support for global highscore-fetching, is TODO atm.

--- src/model/highscore/highscore.lua
+++ src/model/highscore/highscore.lua
@@ -1,11 +1,10 @@
 -- The highscore object.
-Highscore = {playerName = "", score = 0}
+Highscore = {index = 0, playerName = "", score = 0}
 Highscore.__index = Highscore
 
 -- The constructor for the highscore-object
 -- @param playerName - The playername associated with the score
 -- @param score - The score
--- @return Highscore - The object created
 function Highscore:new (playerName, score)
   local self = setmetatable({}, self)
   self.playerName = playerName
