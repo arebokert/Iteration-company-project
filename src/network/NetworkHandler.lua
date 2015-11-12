f455f9cb16d7480fba176cdc8b22b8d0d524bad6
unknown <tomme578@student.liu.se>
2015-11-12 17:26:11 +0100

    Not working version of the addition with the networkhandler...

--- src/network/NetworkHandler.lua
+++ src/network/NetworkHandler.lua
@@ -1,17 +1,49 @@
---
--- Created by IntelliJ IDEA.
--- User: Tomas
--- Date: 2015-11-12
--- Time: 13:39
--- To change this template use File | Settings | File Templates.
---
 socket = require("socket")
-local JSON = assert(loadfile "JSON.lua")()
 
-connection = socket.tcp()
+function operationAbreviation(operation)
+
+    if operation == "QuickStart" then
+        return "QS"
+    else if operation == "SubmitScore" then
+        return "SS"
+    else if operation == "FetchCurrentGames" then
+            return "FG"
+    else if operation == "FetchStatus" then
+            return "FS"
+    else if operation == "TerminateGame" then
+        return "TG"
+    else if operation == "SendScore" then
+            return "SE"
+    else if operation == "RequestReadGlobalScore" then
+        return "RS"
+    else if operation == "RequestReadYourOwnScore" then
+        return "RO"
+    else
+        return error("Invalid input")
+    end
+end
 
 function sendJSON(JSONObject, operation)
-    raw_json_text = operation..JSONObject
-    result2 = assert(connection:send(object), "Message not delivered!")
 
+    --Retrieves the abreviation for the operation
+    operation = operationAbreviation(operation)
+
+    if operation == "Invalid input" then
+        error("Invalid operation input!")
+    end
+
+    --Connect to the server
+    connection = socket.tcp()
+    connection:settimeout(1000)
+    assert(connection:connect("2015-1.pumi.ida.liu.se", 24069), "Connection failed!")
+
+    -- Concatenates the operation with the JSONObject
+    object = operation .. JSONObject
+
+    --Sends the object to the server
+    assert(connection:send(object), "Object could not be delivered!")
+
+    --Closes the connection
+    connection:close()
 end
+
