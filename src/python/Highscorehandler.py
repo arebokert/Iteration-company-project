3bbfd7f805401953912484e12bbab6ec90c95fe4
Gustaf Olofsson <gusol745@student.liu.se>
2015-11-16 08:36:56 +0100

    updated server HighscoreHandler functions. Created the basis for an integration test of local highscorehandler and server

--- src/python/Highscorehandler.py
+++ src/python/Highscorehandler.py
@@ -7,15 +7,20 @@ def addHighscore(jsonObj):
 # https://docs.python.org/2/library/json.html 
 
 # In connectioninterface, use j = json.loads(JSONStringReceivedThroughSocket)
-
+# 
+# return databaseHandler.addHighscore(jsonObj.gameName, jsonObj.score, jsonObj.playername)  Make correct when dbHandler exists.
 	return true
 	
 	
 def retrieveOwnHighscore(jsonObj):
-#TODO implement this 
+#TODO implement this
+# return databaseHandler.retrieveOwnHighscore(jsonObj.gameName, jsonObj.playerName)  Make correct when dbHandler exists. 
+#
+#Alt: j = json.loads(jsonObj), and return databaseHandler.retrieveOwnHighscore(j.gameName, j.playerName)  And same on the rest of the functions
 	return "0"
 	
 def retrieveTopHighscore():
 #TODO implement this
+# return databaseHandler.retrieveTopHighscore()   Make correct when dbHandler exists. Need to jsonify the object somehow or is this done one step up?
 	return "0"
 
