60983d4b5a542a79614ea05dd033729da9d7f102
tomme578 <tomme578@student.liu.se>
2015-11-17 15:38:26 +0100

    Finished the functionality of the NetworkHandler.

--- src/network/NetworkHandler.lua
+++ src/network/NetworkHandler.lua
@@ -1,47 +1,62 @@
 socket = require("socket")
 
+HOST = "2015-1.pumi.ida.liu.se"
+PORT = 24069
+
+    -- Convert the request to the designated prefix which can be found at
+    -- https://docs.google.com/document/d/1OL8HuOFtW8QYHnB0PQ8q-LNm7j-zkNURJ7ER75IaMkk/edit
+    -- @param request - the request to be exectued.
 function convertRequestToPrefix(request)
     if request == "QuickStart" then
         return "QS"
-    else if request == "SubmitScore" then
+    elseif request == "SubmitScore" then
         return "SS"
-    else if request == "FetchCurrentGames" then
-        return "FG"
-    else if request == "FetchStatus" then
+    elseif request == "FetchLastThree" then
+        return "LR/M"
+    elseif request == "FetchStatus" then
         return "FS"
-    else if request == "TerminateGame" then
-        return "TG"
-    else if request == "SendScore" then
+    elseif request == "TerminateMatch" then
+        return "TM"
+    elseif request == "SendScore" then
         return "SE"
-    else if request == "RequestReadGlobalScore" then
+    elseif request == "RequestReadGlobalScore" then
         return "RS"
-    else if request == "RequestReadYourOwnScore" then
+    elseif request == "RequestReadYourOwnScore" then
         return "RO"
     else
         return error("Invalid input")
     end
 end
 
+    -- Send JSONObject with an operation to server.
+    -- Returns the response from the server. The content of the object which is returned
+    -- depends on the request sent.
+    -- @param JSONObject - the JSON object to be sent to the server.
+    -- @param the request to be executed.
 function sendJSON(JSONObject, request)
 
-    --Retrieves the abreviation for the operation
-    requestPrefix = convertRequestToPrefix(request)
+    local serverResponse = "No connection to the internet"
 
-    if requestPrefix == "Invalid input" then
-        error("Invalid operation input!")
+    if hasInternet then
+        ADLogger.trace("Internet connection exists")
+        -- Retrieves the abreviation for the operation
+        requestPrefix = convertRequestToPrefix(request)
+        if requestPrefix == "Invalid input" then
+            error("Invalid operation input!")
+        end
+        -- Connect to the server
+        connection = socket.tcp()
+        connection:settimeout(1000)
+        assert(connection:connect(HOST, PORT), "Connection failed!")
+        -- Concatenates the operation with the JSONObject
+        object = requestPrefix .. JSONObject
+        -- Sends the object to the server
+        assert(connection:send(object), "Object could not be delivered!")
+        serverResponse = assert(connection:receive('*a'), "Nothing received.")
+        -- Closes the connection
+        connection:close()
+        return serverResponse
+    else
+        return serverResponse
     end
-
-    --Connect to the server
-    connection = socket.tcp()
-    connection:settimeout(1000)
-    assert(connection:connect("2015-1.pumi.ida.liu.se", 24069), "Connection failed!")
-
-    -- Concatenates the operation with the JSONObject
-    object = requestPrefix .. JSONObject
-
-    --Sends the object to the server
-    assert(connection:send(object), "Object could not be delivered!")
-
-    --Closes the connection
-    connection:close()
 end
