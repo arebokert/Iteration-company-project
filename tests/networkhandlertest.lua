1255e43f221e3e470e6b10da988b8f584aec08d3
tomme578 <tomme578@student.liu.se>
2015-11-18 10:43:19 +0100

    Added unit test for testconvertRequestToPrefix.

--- tests/networkhandlertest.lua
+++ tests/networkhandlertest.lua
@@ -1,8 +1,15 @@
---
--- Created by IntelliJ IDEA.
--- User: Tomas
--- Date: 2015-11-18
--- Time: 10:23
--- To change this template use File | Settings | File Templates.
---
+package.path = package.path .. ";../src/?.lua"
 
+EXPORT_ASSERT_TO_GLOBALS = true;
+
+require('luaunit') -- always start a test file with this line
+network = require('NetworkHandler')
+
+function testconvertRequestToPrefix(request)
+    assertEquals(network:convertRequestToPrefix("QuickStart"), "QS")
+
+end
+
+
+
+os.exit( LuaUnit.run() ) -- always end a test file with this line
