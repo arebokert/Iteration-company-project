abc55551dab34cff5aade0bef795141e9e6b70cc
GripDidrik <DidrikGrip@c-227-126.eduroam.liu.se>
2015-11-19 11:50:47 +0100

    Added commenting to highscoreview

--- src/model/highscore/highscoreview.lua
+++ src/model/highscore/highscoreview.lua
@@ -1,8 +1,18 @@
 require "model/highscore/highscorehandler"
 
+-- The viewer object, contains a handler, size and position.
 HighscoreViewer = {highscoreHandler = {}, width = 0, height = 0, posX = 0, posY = 0}
 HighscoreViewer.__index = HighscoreViewer
 
+
+-- Constructor for the viewer object.
+-- @param gameName - The name of the game.
+-- @param width - The width of the object, standard is 300px.
+-- @param height - The height of the object, standard is 500px.
+-- @param posX - The x-position of the object.
+-- @param posY - The y-position of the object.
+-- @param fullscreen - A bool deciding if the view is in fullscreen-mode (Not yet implemented!)
+-- @return The object created.
 function HighscoreViewer:new(gameName, width, height, posX, posY, fullscreen)
   local self = setmetatable({}, self)
   self.width = width or 300
@@ -14,19 +24,24 @@ function HighscoreViewer:new(gameName, width, height, posX, posY, fullscreen)
   return self
 end
 
+-- Draws the viewer at the size and position decided when constructed. ATM only split-screen mode is available.
 function HighscoreViewer:draw()
+  -- Background
   screen:fill({r = 0, g = 20, b = 44}, {x = self.posX, y = self.posY, width = self.width, height = self.height})
-  -- Indside border
+  -- Inside border
   screen:fill({r = 255, g = 255, b = 255}, {x = self.posX + 25, y = self.posY + 50, width = self.width - 50, height = self.height - 75})
   screen:fill({r = 0, g = 20, b = 44}, {x = self.posX + 30, y = self.posY + 55, width = self.width - 60, height = self.height - 85})
   
   
   
   fontPath = "/font/Gidole-Regular.otf"
+  -- Text color
   color = {r = 255, g = 255, b = 255}
+  -- Margins is added to the position of the text.
   position = {x = self.posX + 35, y = self.posY + 60}
-  if numberOfHigscores == 3 then 
-    size = self.height - 10
+  if numberOfHigscores == 3 then -- If fullscreen
+    -- TODO: Implement the full-screen mode!
+    --[[size = self.height - 10
 
     score_text = sys.new_freetype(color, size, position, font_path)
     score_text:draw_over_surface(screen, word)
@@ -38,13 +53,13 @@ function HighscoreViewer:draw()
       score_text = sys.new_freetype(color, size, position, font_path)
       score_text:draw_over_surface(screen, word)
       
-    end
-  else
+    end]]
+  else -- If split-screen
+    -- Set the text-size according to the number of rows.
     size = ((self.height - 85) / #self.highscoreHandler.highscoreTable) - 1  
-    print(size)
     highscoreText = sys.new_freetype(color, 30, {x = self.posX + 25, y = self.posY + 10}, fontPath)
     highscoreText:draw_over_surface(screen, "High score")
-        
+    
     for k,v in pairs(self.highscoreHandler.highscoreTable) do
   
       nameText = sys.new_freetype(color, size, position, fontPath)
