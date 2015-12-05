c3d50a5d7827b4e43d032dc4e75b4a885d93eb89
Björn Wilke <bjowi227@student.liu.se>
2015-12-05 16:28:16 +0100

    Fixed another trivial syntax error. Coffee is needed.

--- src/python/database_handler.py
+++ src/python/database_handler.py
@@ -964,9 +964,9 @@ def add_highscore(gamename, mac, playerid, score):
         global_id = add_user(mac, playerid)
         if global_id == -1:
             global_id = get_user(mac, playerid)
-                if global_id == -1:
-                    log_error('add_highscore', 'Something went wrong.')
-                    return -1
+            if global_id == -1:
+                log_error('add_highscore', 'Something went wrong.')
+                return -1
 
     c = get_db()
     try:
